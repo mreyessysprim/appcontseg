432d43ce6b6047835b613c9145d614cdd5f37099
mreyes <mreyes.ceo@sysprim.com>
2024-05-15 13:46:30 -0400

    seed user
    Creacion de usarios ramdon

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,9 +1,14 @@
 <?php
 
 namespace Database\Seeders;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+//use Illuminate\App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,12 +17,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        //     'name' => 'Administrador',
+        //     'email' => 'admin@gmail.com',
+        //     'password' => Hash::make("12345678"),
         // ]);
-        $this->call(UserSeeder::class);
+        //$this->call(UserSeeder::class);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\App\Models\User;
 
 class UserSeeder extends Seeder
@@ -14,11 +17,14 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'name'=>'administrador',
-            'email'=>'admin@gmail.com',
-            'password'=>Hash::make("12345678")
+        User :: create([
+            'name' => 'administrador',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make("12345678"),
         ]);
+        
+        User::factory(15)->create();
+
         //
     }
 }
