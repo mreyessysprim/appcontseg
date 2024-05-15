066aad4337ff2b96d0b4de9f783ad22ae46f94dd
mreyes <mreyes.ceo@sysprim.com>
2024-05-15 12:47:19 -0400

    creacion seeder
    se creo el seeder para crear usuarios

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name'=>'administrador',
+            'email'=>'admin@gmail.com',
+            'password'=>Hash::make("12345678")
+        ]);
+        //
+    }
+}
