cf2fbca886a3bdb7e45f87033613f65095b286ba
mreyes <mreyes.ceo@sysprim.com>
2024-05-15 11:36:52 -0400

    comit adminlte
    instalando la plantilla AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,8 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
-});
-// ->middleware(['auth', 'verified'])->name('dashboard');
+    return view('dashboard')
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
